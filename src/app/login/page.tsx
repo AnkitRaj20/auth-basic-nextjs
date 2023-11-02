e267987762e5c840890ed30e79d9fe70d45f6c54
Ankit Raj <85890299+AnkitRaj20@users.noreply.github.com>
2023-11-02 13:46:26 +0530

    login completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,67 +1,162 @@
+// "use client";
+// import Link from "next/link";
+// import React from "react";
+// import {useRouter} from "next/navigation";
+// import axios from "axios";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+
+// export default function signup() {
+//   const router = useRouter();
+//   const [user, setUser] = React.useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const [loading,setLoading] = React.useState(false)
+
+//   const onLogin = async () => {
+//     try {
+//       setLoading(true);
+//       const response = await axios.post("/api/users/login",user);
+//       console.log("Login Success",response.data)
+//       alert("Success")
+//       router.push("/profile")
+//     } catch (error:any) {
+//       alert("Error")
+//       console.log("Login Failed",error.message);
+//     }
+//     finally{
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+//       <h1 className="text-align-center">Login</h1>
+
+//       <Form>
+//         <Form.Group className="mb-3">
+//           <Form.Label>Email address</Form.Label>
+//           <Form.Control
+//             type="email"
+//             placeholder="Enter email"
+//             id="email"
+//             value={user.email}
+//             onChange={(e) =>
+//               setUser({
+//                 ...user,
+//                 email: e.target.value,
+//               })
+//             }
+//           />
+//           <Form.Text className="text-muted">
+//             We'll never share your email with anyone else.
+//           </Form.Text>
+//         </Form.Group>
+
+//         <Form.Group className="mb-3">
+//           <Form.Label>Password</Form.Label>
+//           <Form.Control
+//             type="password"
+//             placeholder="Password"
+//             id="password"
+//             value={user.password}
+//             onChange={(e) =>
+//               setUser({
+//                 ...user,
+//                 password: e.target.value,
+//               })
+//             }
+//           />
+//         </Form.Group>
+
+//         <Button variant="primary" type="submit" onClick={onLogin}>
+//           Login
+//         </Button>
+//       </Form>
+//       <Link href="/signup">Signup</Link>
+//     </div>
+//   );
+// }
+
+
 "use client";
 import Link from "next/link";
-import React from "react";
-import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import React, {useEffect} from "react";
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 
-export default function signup() {
-  const [user, setUser] = React.useState({
-    email: "",
-    password: "",
-  });
 
-  const onLogin = async () => {};
 
-  return (
+export default function LoginPage() {
+    const router = useRouter();
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+       
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data);
+            toast.success("Login success");
+            router.push("/profile");
+        } catch (error:any) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        } finally{
+        setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else{
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
+    return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-align-center">Login</h1>
-
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
+        <h1>{loading ? "Processing" : "Login"}</h1>
+        <hr />
+        
+        <label htmlFor="email">email</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="text"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+        <label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="password"
-            value={user.password}
-            onChange={(e) =>
-              setUser({
-                ...user,
-                password: e.target.value,
-              })
-            }
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
             type="password"
-            placeholder="Password"
-            id="password"
             value={user.password}
-            onChange={(e) =>
-              setUser({
-                ...user,
-                password: e.target.value,
-              })
-            }
-          />
-        </Form.Group>
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+            <button
+            onClick={onLogin}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
+            <Link href="/signup">Visit Signup page</Link>
+        </div>
+    )
 
-        <Button variant="primary" type="submit" onClick={onLogin}>
-          Login
-        </Button>
-      </Form>
-      <Link href="/signup">Signup</Link>
-    </div>
-  );
 }
