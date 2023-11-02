1916ad5de60a09e974d0adc15d1b784e2ee8a925
Ankit Raj <85890299+AnkitRaj20@users.noreply.github.com>
2023-11-02 14:01:46 +0530

    logout completed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,22 @@
+"use client"
+import axios from "axios"
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage(){
+    const router = useRouter();
+    const logout = async() => {
+        await axios.get('/api/users/logout');
+        alert('logged out');
+        router.push('/login');
+    }
+
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
+
+            <button className="bg-blue-500 hover:bg-blue-800 text-white p-5 mt-3 rounded" onClick={logout}>
+                LogOut
+            </button>
         </div>
     )
 }
